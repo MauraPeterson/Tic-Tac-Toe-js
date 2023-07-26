a0ea658b5e7e0825b619a083fe4c36f692fc4b6d
MauraPeterson <maurapetersonsoftware@gmail.com>
2023-07-26 10:37:23 -0600

    Add play against computer

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -1,14 +1,34 @@
 let gameBoard;
+let winMove;
 let gameWon;
 let currentPlayer;
+let ai = 'O';
+let isAI = false;
 
-document.querySelector('.js-start-button')
-  .addEventListener('click', game)
+document.querySelector('.js-2-player-button')
+  .addEventListener('click', game2Player);
 
-function game(){
+document.querySelector('.js-ai-button')
+  .addEventListener('click', gameAI);
+
+function gameAI(){
+  isAI = true;
   gameWon = false;
   resetBoard();
   renderBoard();
+
+  currentPlayer = pickFirstPlayer();
+  if(currentPlayer === ai){
+    getComputerMove();
+  }
+}
+
+function game2Player(){
+  gameWon = false;
+  resetBoard();
+  renderBoard();
+
+  isAI = false;
 
   renderTurnText(pickFirstPlayer());  
 }
@@ -24,6 +44,11 @@ function pickFirstPlayer(){
 
 function renderTurnText(player){
   currentPlayer = player;
+  if(player === 'Tie'){
+    document.querySelector('.js-turn-text')
+      .innerHTML = `Tie`;
+      return;
+  }
   if(gameWon){
     document.querySelector('.js-turn-text')
       .innerHTML = `Player ${currentPlayer} Won`;
@@ -39,25 +64,45 @@ function choice(index){
   console.log(gameBoard);
   checkWin();
   renderBoard();
+  if(isAI){
+    getComputerMove();
+  }
   
 }
 
 function renderBoard(){
-  document.querySelector('.js-game-board')
+  if(!gameWon){
+    document.querySelector('.js-game-board')
     .innerHTML = getHtml();
 
-  if(!gameWon){
     document.querySelectorAll('.js-game-button').forEach((element, index) => element
     .addEventListener('click', function gameClick(){ choice(index)}));
+  } else {
+    document.querySelector('.js-game-board')
+    .innerHTML = getHtmlWin();
   }
 }
 
 function getHtml(){
   let html = '';
-  let i = 0;
   gameBoard.forEach((element) => {
-    i++;
-    html += `<button class="game-button js-game-button">${element}</button>`;
+    html += `<button class="game-button-play game-button js-game-button">${element}</button>`;
+
+  });
+  return html;
+}
+
+function getHtmlWin(){
+  let html = '';
+  const win1 = winMove[0];
+  const win2 = winMove[1];
+  const win3 = winMove[2];
+  gameBoard.forEach((element, i) => {
+    if(i === win1 || i === win2 || i === win3){
+      html += `<button class="game-button-win-move game-button js-game-button">${element}</button>`;
+    } else{
+      html += `<button class="game-button-win game-button js-game-button">${element}</button>`;
+    }
 
   });
   return html;
@@ -76,7 +121,11 @@ function checkWin(){
   if(checkColumns() || checkRows() || checkDiaganals()){
     gameWon = true;
     renderTurnText(currentPlayer);
-  } else{
+  } else if(gameTie()){
+    gameWon = true;
+    winMove = [null, null, null];
+    renderTurnText('Tie');
+  } else {
     renderTurnText(nextPlayer(currentPlayer));
   }
 }
@@ -87,10 +136,12 @@ function checkDiaganals(){
   }
   if(gameBoard[0] === gameBoard[4] 
     && gameBoard[4] === gameBoard[8]){
-    return true;
+      winMove = [0, 4, 8];
+      return true;
   } else if(gameBoard[2] === gameBoard[4] 
     && gameBoard[4] === gameBoard[6]){
-    return true;
+      winMove = [2, 4, 6];
+      return true;
   } else {
     return false;
   }
@@ -103,6 +154,7 @@ function checkRows(){
     if(gameBoard[0 + row] === gameBoard[1 + row] 
       && gameBoard[1 + row] === gameBoard[2 + row]
       && gameBoard[0 + row] !== ''){
+        winMove = [0 + row, 1 + row, 2 + row];
       return true;
     }
   }
@@ -116,6 +168,7 @@ function checkColumns(){
     if(gameBoard[0 + i] === gameBoard[3 + i] 
       && gameBoard[3 + i] === gameBoard[6 + i]
       && gameBoard[0 + i] !== ''){
+      winMove = [0 + i, 3 + i, 6 + i];
       return true;
     }
   }
@@ -123,8 +176,44 @@ function checkColumns(){
   return false;
 }
 
+function gameTie(){
+  let isTie = true;
+  gameBoard.forEach((element) => {
+    if(element === ''){
+      isTie = false;
+    }
+  });
+  return isTie;
+}
+
 function resetBoard(){
   gameBoard = ['', '', '',
               '', '', '',
               '', '', ''];
+}
+
+function getComputerMove(){
+  let bestScore = -Infinity;
+  let bestMove;
+  gameBoard.forEach((element, i) => {
+    // Is spot available?
+    if(element === ''){
+      gameBoard[i] = 'O';
+      let score = minimax(gameBoard);
+      gameBoard[i] = '';
+      // Is spot best?
+      if(score > bestScore){
+        bestScore = score;
+        bestMove = i;
+      }
+    }
+  });
+  // Play best move
+  gameBoard[bestMove] = 'O';
+  renderBoard();
+  checkWin();
+}
+
+function minimax(board){
+  return 1;
 }
